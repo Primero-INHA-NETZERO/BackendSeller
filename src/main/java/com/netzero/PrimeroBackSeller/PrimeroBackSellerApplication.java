a996224768b7e45f9892d18f6eb831a1c93e4a74
Jamey <musoyou1085@gmail.com>
2024-07-19 13:50:06 +0900

    first commit

---
+++ src/main/java/com/netzero/PrimeroBackSeller/PrimeroBackSellerApplication.java
@@ -0,0 +1,13 @@
+package com.netzero.PrimeroBackSeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeroBackSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeroBackSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netzero/PrimeroBackSeller/PrimeroBackSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netzero.PrimeroBackSeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeroBackSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
