6eb4886b093c49f91246878f67c0ba4e2bcea714
haazz <gkwldnjs121@gmail.com>
2024-07-20 09:13:57 +0900

    fix

--- src/main/java/com/netzero/PrimeroBackSeller/repository/PurchaseRepository.java
+++ src/main/java/com/netzero/PrimeroBackSeller/repository/PurchaseRepository.java
@@ -3,10 +3,13 @@ package com.netzero.PrimeroBackSeller.repository;
 import com.netzero.PrimeroBackSeller.domain.Product;
 import com.netzero.PrimeroBackSeller.domain.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
+    @Query
     List<Purchase> findByConsumerId(Long id);
 
 }
